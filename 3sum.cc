08541f3576f9fcb054926758b55ec0d4ecbd5801
dantin <chengjie.ding@gmail.com>
2023-09-13 14:14:40 +0800

    Add solutions.

--- 3sum.cc
+++ 3sum.cc
@@ -14,14 +14,78 @@ public:
         sort(nums.begin(), nums.end());
 
         for (size_t i = 0; i < nums.size() - 2; i++) {
-
+            size_t j = i + 1;
+            size_t k = nums.size() - 1;
+            while (j < k) {
+                vector<int> pos;
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    pos.push_back(nums[i]);
+                    pos.push_back(nums[j]);
+                    pos.push_back(nums[k]);
+                    retval.push_back(pos);
+                    ++j;
+                    --k;
+                    while (j < k && nums[j - 1] == nums[j]) {
+                        ++j;
+                    }
+                    while (j < k && nums[k] == nums[k+1]) {
+                        --k;
+                    }
+                } else if(nums[i] + nums[j] + nums[k] < 0) {
+                    ++j;
+                } else {
+                    --k;
+                }
+            }
+            while (i < nums.size() - 1 && nums[i] == nums[i + 1]) {
+                ++i;
+            }
         }
 
         return retval;
     }
 };
 
+void print_vector(vector<int> nums)
+{
+    cout << "[";
+    for (auto it = nums.begin(); it != nums.end(); ++it) {
+        cout << *it;
+        if ((it + 1) != nums.end()) {
+            cout << ", ";
+        }
+    }
+    cout << "]";
+}
+
+void print_result(vector<vector<int>> nums)
+{
+    cout << "[" << endl;
+    for (auto it = nums.begin(); it != nums.end(); ++it) {
+        cout << "   ";
+        print_vector(*it);
+        if ((it + 1) != nums.end()) {
+            cout << ", ";
+        }
+        cout << endl;
+    }
+    cout << "]";
+}
+
 int main()
 {
+    Solution s;
+
+    vector<int> nums{-1, 0, 1, 2, -1, -4};
+    cout << "Input: " << endl;
+    print_vector(nums);
+    cout << endl;
+
+    auto retval = s.threeSum(nums);
+
+    cout << "Output: " << endl;
+    print_result(retval);
+    cout << endl;
+
     return 0;
 }
