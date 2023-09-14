f3dad959a0285319fc7cda9cdb43c3f0e35e7899
dantin <chengjie.ding@gmail.com>
2023-09-14 13:18:21 +0800

    Add solutions.

--- remove_nth_node_from_end_of_list.cc
+++ remove_nth_node_from_end_of_list.cc
@@ -1,7 +1,50 @@
 #include <iostream>
+
+#include "singly_linked_list.h"
+
 using namespace std;
+
+class Solution {
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n) {
+        ListNode dummy;
+        dummy.next = head;
+
+        int len = 0;
+        ListNode *first = head;
+        while (first != nullptr) {
+            len++;
+            first = first->next;
+        }
+
+        len = len - n;
+        first = &dummy;
+        while (len > 0) {
+            len--;
+            first = first->next;
+        }
+
+        first->next = first->next->next;
+        //delete first;
+
+        return dummy.next;
+    }
+};
 
 int main()
 {
+    Solution s;
+    ListNode *list = build_by_num(54321);
+    int n = 2;
+    cout << "Input: ";
+    print_list(list);
+    cout << ", n = " << n << endl;
+
+    ListNode *retval = s.removeNthFromEnd(list, n);
+
+    cout << "Output: ";
+    print_list(retval);
+    cout << endl;
+
     return 0;
 }
--- singly_linked_list.h
+++ singly_linked_list.h
@@ -3,10 +3,15 @@
 
 #include <iostream>
 
+/**
+ * Definition for singly-linked list.
+ */
 struct ListNode {
     int val;
     ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 ListNode *build_by_num(int n)
