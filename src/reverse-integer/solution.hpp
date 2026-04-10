851ad8060734b8d33c6dd58863ed08f2872d5e7f
dantin <chengjie.ding@gmail.com>
2026-04-10 17:19:44 +0800

    update code.

---
+++ src/reverse-integer/solution.hpp
@@ -0,0 +1,31 @@
+#ifndef SOLUTION_HPP
+#define SOLUTION_HPP
+
+class Solution {
+   public:
+    int reverse(int x)
+    {
+        int retval = 0;
+        const int MAX_DIV_10 = 214748364; // INT_MAX / 10
+        const int MIN_DIV_10 = -214748364; // INT_MIN / 10
+
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+
+            // Check for overflow before multiplying by 10 and adding pop
+            if (retval > MAX_DIV_10 || (retval == MAX_DIV_10 && pop > 7)) {
+                return 0;
+            }
+            if (retval < MIN_DIV_10 || (retval == MIN_DIV_10 && pop < -8)) {
+                return 0;
+            }
+
+            retval = retval * 10 + pop;
+        }
+
+        return retval;
+    }
+};
+
+#endif
